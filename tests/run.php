507e2e08a96fccfab7f99997b0fc8e7ceac57a25
Ava Plataforma <avaplataforma@users.noreply.github.com>
2026-08-05 14:00:17 -0300

    Use unified course selection for enrollments

--- tests/run.php
+++ tests/run.php
@@ -571,6 +571,7 @@ $tests['prepara fluxo unificado de matrículas'] = static function () use ($root
     assertTrue(is_file($rootPath.'/modules/Finance/CampaignRepository.php'));
     assertTrue(is_file($rootPath.'/database/migrations/20260805_630000_add_max_installments_to_finance_products.php'));
     assertTrue(is_file($rootPath.'/database/migrations/20260805_640000_link_finance_products_to_moodle_courses.php'));
+    assertTrue(str_contains((string)file_get_contents($rootPath.'/views/moodle/enrollments/form.php'),'1. Curso contratado'));
     assertTrue(str_contains($layout,'>Cadastro</a>'));
 };
 
