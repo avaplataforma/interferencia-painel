88acc759251d63400573ddbf5300e321c441999b
Ava Plataforma <avaplataforma@users.noreply.github.com>
2026-08-12 17:43:47 -0300

    fix(moodle): liberar publicação de trilhas no conector

--- integrations/moodle/local_mundointer/version.php
+++ integrations/moodle/local_mundointer/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_mundointer';
-$plugin->version = 2026081201;
+$plugin->version = 2026081202;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.6.0';
+$plugin->release = '0.6.1';
