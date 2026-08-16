cd14b20c8f4d7636c3b1db91da4a1285f60375a9
AVA Plataforma® <jonathan.jw.oliveira@gmail.com>
2026-08-16 17:43:32 -0300

    release: plugin Mundo Inter 0.9.50

--- integrations/moodle/local_mundointer/version.php
+++ integrations/moodle/local_mundointer/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_mundointer';
-$plugin->version = 2026081617;
+$plugin->version = 2026081618;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.9.49';
+$plugin->release = '0.9.50';
